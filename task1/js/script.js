0f66193647f10f28ccbbde2776e4420f8cfa2572
denissemo <denissemo1999@gmail.com>
2019-03-29 23:40:52 +0200

    add area limit

--- task1/js/script.js
+++ task1/js/script.js
@@ -5,6 +5,7 @@ let area = document.getElementById("area");
 for (let r of rectangles) {
     // mousedown listener for all rectangles
     r.addEventListener("mousedown", function (e) {
+        let area_coords = area.getBoundingClientRect();
         let coords = getCoords(r);
         let shiftX = e.pageX - coords.left;
         let shiftY = e.pageY - coords.top;
@@ -18,8 +19,23 @@ for (let r of rectangles) {
         function move(e) {
             let move_x = e.pageX - shiftX;
             let move_y = e.pageY - shiftY;
-            r.style.left = `${move_x}px`;
-            r.style.top = `${move_y}px`;
+            if (move_x < 0) {
+                r.style.left = "0px";
+            } else {
+                r.style.left = `${move_x}px`;
+            }
+            if (move_y < 0) {
+                r.style.top = "0px";
+            } else {
+                r.style.top = `${move_y}px`;
+            }
+            // when rectangle move to bottom and right sides of area
+            if (move_x > area_coords.width - coords.box.width) {
+                r.style.left = `${area_coords.width - coords.box.width}px`;
+            }
+            if (move_y > area_coords.height - coords.box.height) {
+                r.style.top = `${area_coords.height - coords.box.height}px`;
+            }
         }
 
         document.onmousemove = function (e) {
@@ -35,14 +51,15 @@ for (let r of rectangles) {
 
     function getCoords(element) {
         let box = element.getBoundingClientRect();
-        console.log(area.getBoundingClientRect());
-        // console.log(box);
+        console.log(box);
         return {
             top: box.top + pageYOffset,
-            left: box.left + pageXOffset
+            left: box.left + pageXOffset,
+            box: box
         };
     }
 
+    // disable browser drag and drop
     r.addEventListener("dragstart", function () {
         return false;
     });
